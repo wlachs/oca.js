377e17a58093be1e58e180f37b6b817643df6618
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-07-11 18:01:33 +0200

    remove db initial data, to be implemented in another module

--- server/src/layout/index.js
+++ server/src/layout/index.js
@@ -1,11 +1,9 @@
 import log from 'npmlog';
-import initDB from './db';
 
 const LOG_PREFIX = 'LAYOUT';
 
 async function init() {
   log.info(LOG_PREFIX, 'init layout module');
-  await initDB();
 }
 
 export default init;
