81f85019d33758124acb03942c9775eca8e3e231
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-07-13 22:52:58 +0200

    route path regex validation

---
+++ server/src/layout/dao/validators/route_validator.js
@@ -0,0 +1,20 @@
+import log from 'npmlog';
+
+const LOG_PREFIX = 'LAYOUT_DAO_VALIDATE_ROUTE';
+const ROUTE_REGEX = /^(\/(([a-zA-Z]*)|(\?[a-zA-Z]+)))+$/;
+
+function validate(route) {
+  log.info(LOG_PREFIX, 'validate route:', JSON.stringify(route));
+
+  const { path } = route;
+  log.verbose(LOG_PREFIX, 'match route path with regex', path, ROUTE_REGEX);
+
+  if (path.match(ROUTE_REGEX)) {
+    log.info(LOG_PREFIX, 'route valid', JSON.stringify(route));
+  } else {
+    log.error(LOG_PREFIX, 'route invalid', JSON.stringify(route));
+    throw new Error('route validation failed, route invalid');
+  }
+}
+
+export default validate;
