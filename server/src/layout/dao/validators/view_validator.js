57c2e37cb3c50fea3f654081569d9a189a820262
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-07-13 17:24:14 +0200

    implement view validator

---
+++ server/src/layout/dao/validators/view_validator.js
@@ -0,0 +1,39 @@
+import log from 'npmlog';
+
+const LOG_PREFIX = 'LAYOUT_DAO_VALIDATE_VIEW';
+
+function validateSlotContentAssignment(slot, content) {
+  log.verbose(LOG_PREFIX, 'validate slot->content assignment', JSON.stringify(slot), JSON.stringify(content));
+
+  const { key, allowedContentTypes } = slot;
+  const assignment = content.find((contentAssignment) => contentAssignment.slot === key);
+
+  if (!assignment) {
+    log.error(LOG_PREFIX, 'no assigned content found for slot:', slot.key);
+    return false;
+  }
+
+  if (allowedContentTypes.indexOf(assignment.content.type) !== -1) {
+    log.verbose(LOG_PREFIX, 'valid slot->content association:', slot.key, assignment.content.key);
+    return true;
+  }
+
+  return false;
+}
+
+function validate(view) {
+  log.info(LOG_PREFIX, 'validate view:', JSON.stringify(view));
+
+  const { template, content } = view;
+  const templateSlots = template.slots;
+
+  /* Find for each template slot the matching content association in the content map */
+  if (templateSlots.every((slot) => validateSlotContentAssignment(slot, content))) {
+    log.info(LOG_PREFIX, 'view valid', JSON.stringify(view));
+  } else {
+    log.error(LOG_PREFIX, 'view invalid', JSON.stringify(view));
+    throw new Error('view validation failed, view invalid');
+  }
+}
+
+export default validate;
