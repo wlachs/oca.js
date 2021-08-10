6388afe4c6541380df6b45df61c8e48fa21f768c
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-08-10 12:00:20 +0200

    add missing log

--- server/src/core/dao/index.js
+++ server/src/core/dao/index.js
@@ -1,6 +1,10 @@
+import log from 'npmlog';
 import { removeApplicationProperties } from './application_property';
+
+const LOG_PREFIX = 'CORE_DAO';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function clearDB() {
+  log.info(LOG_PREFIX, 'clear db');
   await removeApplicationProperties();
 }
