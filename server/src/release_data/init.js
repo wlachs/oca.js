a7eebdd330e7f05510932420cc580c0913c11ee7
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-08-10 12:00:37 +0200

    first implementation of db wipe

--- server/src/release_data/init.js
+++ server/src/release_data/init.js
@@ -4,3 +4,14 @@
 * * delete everything from the DB
 * * load each file from the initial_data folder
 */
+
+import log from 'npmlog';
+import initDB from '../core/db';
+import wipeDB from './services/wipe_db';
+
+const LOG_PREFIX = 'RELEASE_DATA_INIT';
+
+log.info(LOG_PREFIX, 'start initialisation');
+initDB()
+  .then(wipeDB)
+  .catch((err) => log.error(LOG_PREFIX, JSON.stringify(err)));
