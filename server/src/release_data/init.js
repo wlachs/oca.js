308e3dff7ed639426bceda5ea94c52242f4c6ea4
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-08-10 11:36:19 +0200

    clear for now empty init and update files

---
+++ server/src/release_data/init.js
@@ -0,0 +1,6 @@
+/*
+* Plan:
+* * connect to DB
+* * delete everything from the DB
+* * load each file from the initial_data folder
+*/
---
+++ server/src/release_data/update.js
@@ -0,0 +1,5 @@
+/*
+* Plan:
+* * connect to DB
+* * load each file from the release_data subfolders which are marked to be loaded
+*/
