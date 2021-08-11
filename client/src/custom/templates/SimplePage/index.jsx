5c410b312947de550e3386712fb3f98dc019824a
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-08-11 15:51:54 +0200

    update SimplePage css classes

--- client/src/custom/templates/SimplePage/index.jsx
+++ client/src/custom/templates/SimplePage/index.jsx
@@ -7,7 +7,7 @@ import Slot from '../../../core/containers/Slot';
 
 function SimplePage() {
   return (
-    <div className="container-fluid" id="SimplePage-Template">
+    <div className="container-fluid col-12 m-0 p-0 min-vh-100 py-4 d-flex flex-column">
       <Slot slotKey="TOP_BAR_SLOT" />
       <Slot slotKey="PAGE_HEADER_SLOT" />
       <Slot slotKey="MAIN_CONTENT_SLOT" />
