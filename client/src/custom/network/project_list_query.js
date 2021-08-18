eb8c0103d86943980946e9279badc0aea78fb042
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-08-18 15:06:40 +0200

    remove obsolete todo

--- client/src/custom/network/project_list_query.js
+++ client/src/custom/network/project_list_query.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { query } from 'gql-query-builder';
 import { apiEndpoint } from '../../core/config/api_config';
 
-/* TODO: replace API URL */
 async function projectListQuery() {
   const response = await axios.post(apiEndpoint(), query({
     operation: 'projects',
