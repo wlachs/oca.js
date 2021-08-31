b5d1920d291dc8718ec6252f9d6eb98a426637c8
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-09-01 00:06:29 +0200

    delayed execution of route API calls on DEV to simulate real-life conditions

--- client/src/core/network/route_query.js
+++ client/src/core/network/route_query.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { query } from 'gql-query-builder';
 import { apiEndpoint } from '../config/api_config';
+import { DEVELOPMENT } from '../config/environment';
+import returnAfter from '../utils/delayed_execution';
 
 async function routeQuery(path) {
   const response = await axios.post(apiEndpoint(), query({
@@ -35,6 +37,11 @@ async function routeQuery(path) {
         ],
       }],
   }));
+
+  /* Introduce delay on dev */
+  if (process.env.NODE_ENV === DEVELOPMENT) {
+    return returnAfter(response.data.data.route, 1000);
+  }
 
   return response.data.data.route;
 }
