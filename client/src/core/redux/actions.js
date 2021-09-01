32eb6445e310b40da51723692e3a28a837efecfd
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-09-02 01:28:37 +0200

    implement authentication service with redux support

--- client/src/core/redux/actions.js
+++ client/src/core/redux/actions.js
@@ -6,10 +6,14 @@ import {
   ROUTE_QUERY_START,
   ROUTE_QUERY_SUCCESS,
   SET_LOADING_STATE,
-  SET_PATH,
+  LOGIN_ERROR,
+  LOGIN_START,
+  LOGIN_SUCCESS,
 } from './action_types';
 import defaultRouteQuery from '../network/default_route_query';
 import routeQuery from '../network/route_query';
+
+import authentication from '../network/authentication';
 
 export function setLoadingState(state) {
   return (dispatch) => dispatch({
@@ -62,9 +66,28 @@ export function getRoute(path) {
   };
 }
 
-export function setPath(value) {
-  return (dispatch) => dispatch({
-    SET_PATH,
-    value,
-  });
+export function login(userID, password) {
+  return (dispatch, getState) => {
+    const state = getState();
+
+    /* Start login request */
+    dispatch({
+      type: LOGIN_START,
+    });
+
+    authentication(userID, password, state.core.route.path)
+      /* Login successful */
+      .then((value) => dispatch({
+        type: LOGIN_SUCCESS,
+        value,
+      }))
+
+      /* Login failed */
+      .catch((value) => {
+        dispatch({
+          type: LOGIN_ERROR,
+          value,
+        });
+      });
+  };
 }
--- client/src/core/redux/reducer.js
+++ client/src/core/redux/reducer.js
@@ -1,19 +1,18 @@
 import {
   DEFAULT_ROUTE_QUERY_ERROR,
   DEFAULT_ROUTE_QUERY_START,
-  DEFAULT_ROUTE_QUERY_SUCCESS,
+  DEFAULT_ROUTE_QUERY_SUCCESS, LOGIN_ERROR, LOGIN_START, LOGIN_SUCCESS,
   ROUTE_QUERY_ERROR,
   ROUTE_QUERY_START,
   ROUTE_QUERY_SUCCESS,
   SET_LOADING_STATE,
-  SET_PATH,
 } from './action_types';
 
 const INITIAL_STATE = {
   loading: null,
   error: null,
   route: null,
-  path: null,
+  bearer: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -55,10 +54,24 @@ export default (state = INITIAL_STATE, action) => {
         error: action.value,
       };
 
-    case SET_PATH:
+    case LOGIN_START:
       return {
         ...state,
-        path: action.value,
+        loading: true,
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        bearer: action.value.bearer,
+      };
+
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.value,
       };
 
     default:
