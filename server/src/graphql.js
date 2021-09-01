fb801e3eb07946c3a5e1a0250df3c9c06e2afa55
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-09-01 20:35:03 +0200

    add redirect gql api to the admin endpoint

--- server/src/graphql.js
+++ server/src/graphql.js
@@ -14,6 +14,7 @@ import { RouteMutation, RouteQuery } from './layout/graphql/route';
 import { ProjectMutation, ProjectQuery } from './projects/graphql/project';
 import { UserMutation, UserQuery } from './auth/graphql/user';
 import { TokenQuery } from './auth/graphql/token';
+import { RedirectMutation, RedirectQuery } from './auth/graphql/redirect';
 
 /* Misc */
 import { conditionalMW } from './utils/express-utils';
@@ -44,6 +45,7 @@ const AdminQueryType = new GraphQLObjectType({
     ...RouteQuery,
     ...ProjectQuery,
     ...UserQuery,
+    ...RedirectQuery,
   },
 });
 
@@ -60,6 +62,7 @@ const AdminMutationType = new GraphQLObjectType({
     ...RouteMutation,
     ...ProjectMutation,
     ...UserMutation,
+    ...RedirectMutation,
   },
 });
 
