cc7b3e1bc2f01af02183800003aa93fde3c8e054
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-09-06 17:02:44 +0200

    cleanup core constants.js

--- server/src/core/constants.js
+++ server/src/core/constants.js
@@ -1,22 +1,4 @@
 /**
- * Key of the admin username application property
- * @type {string}
- */
-export const ADMIN_USERNAME_KEY = 'ADMIN_USERNAME';
-
-/**
- * Default admin username
- * @type {string}
- */
-export const ADMIN_USERNAME_DEFAULT = 'admin';
-
-/**
- * Default admin password
- * @type {string}
- */
-export const ADMIN_PASSWORD_DEFAULT = 'admin';
-
-/**
  * Default application route key
  * @type {string}
  */
