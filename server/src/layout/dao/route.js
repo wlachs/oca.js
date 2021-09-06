c0190eb58db8d6c97535ae3e73b3f45bb473598c
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-09-06 21:38:34 +0200

    return fallback routes if applicable

--- server/src/layout/dao/route.js
+++ server/src/layout/dao/route.js
@@ -19,8 +19,48 @@ import { getUserGroupsByKeys } from '../../auth/dao/user_group';
 /* Utils */
 import filterAccessibleRoutes from './utils/filter_accessible_routes';
 
+/* Constants */
+import { NOT_AUTHORIZED_ROUTE_PATH, NOT_FOUND_ROUTE_PATH } from '../constants';
+
+/* Errors */
+import NotFoundError from './errors/not_found';
+import NotAuthorizedError from './errors/not_authorized';
+import RouteAlreadyExistsError from './errors/route_already_exists';
+
 /* Logging prefix */
 const LOG_PREFIX = 'LAYOUT_DAO_ROUTE';
+
+async function get404Route() {
+  log.info(LOG_PREFIX, 'get 404 route - not found');
+
+  const route = await RouteModel
+    .findOne({ path: NOT_FOUND_ROUTE_PATH })
+    .populate(POPULATE_ROUTE_FULL);
+
+  if (!route) {
+    log.info(LOG_PREFIX, 'no 404 route found with path:', NOT_FOUND_ROUTE_PATH);
+    throw new NotFoundError(`can't get route, no route found with path: ${NOT_FOUND_ROUTE_PATH}`);
+  }
+
+  log.verbose(LOG_PREFIX, jsonifyRoute(route));
+  return route;
+}
+
+async function get401Route() {
+  log.info(LOG_PREFIX, 'get 401 route - unauthorized');
+
+  const route = await RouteModel
+    .findOne({ path: NOT_AUTHORIZED_ROUTE_PATH })
+    .populate(POPULATE_ROUTE_FULL);
+
+  if (!route) {
+    log.info(LOG_PREFIX, 'no 401 route found with path:', NOT_AUTHORIZED_ROUTE_PATH);
+    throw new NotAuthorizedError(`can't get route, no route found with path: ${NOT_AUTHORIZED_ROUTE_PATH}`);
+  }
+
+  log.verbose(LOG_PREFIX, jsonifyRoute(route));
+  return route;
+}
 
 export async function getRouteByPath(path, user) {
   log.info(LOG_PREFIX, 'get route by path:', path);
@@ -30,8 +70,8 @@ export async function getRouteByPath(path, user) {
     .populate(POPULATE_ROUTE_FULL);
 
   if (!route) {
-    log.error(LOG_PREFIX, 'no route found with path:', path);
-    throw new Error(`can't get route, no route found with path: ${path}`);
+    log.info(LOG_PREFIX, 'no route found with path:', path);
+    throw new NotFoundError(`can't get route, no route found with path: ${path}`);
   }
 
   /* If the function is called by the system upon initialization, there is no user object added */
@@ -42,12 +82,31 @@ export async function getRouteByPath(path, user) {
   const accessGroups = route.accessGroups.map((g) => g.key);
   const hasAccess = await isMemberOrAnyGroup(user.groups, accessGroups);
   if (!hasAccess) {
-    log.error(LOG_PREFIX, 'user has no access to route:', path);
-    throw new Error(`can't get route, user has no access to route: ${path}`);
+    log.info(LOG_PREFIX, 'user has no access to route:', path);
+    throw new NotAuthorizedError(`can't get route, user has no access to route: ${path}`);
   }
 
   log.verbose(LOG_PREFIX, jsonifyRoute(route));
   return route;
+}
+
+export async function getRouteByPathOrDefault(path, user) {
+  log.info(LOG_PREFIX, 'get route by path or default', path, user);
+
+  try {
+    return await getRouteByPath(path, user);
+  } catch (e) {
+    /* If the route is not found */
+    if (e instanceof NotFoundError) {
+      return get404Route();
+    }
+    /* If the user is not authorized to see the route */
+    if (e instanceof NotAuthorizedError) {
+      return get401Route();
+    }
+    /* If none of the custom error types match */
+    throw e;
+  }
 }
 
 async function getRouteByPathOrNull(path, user) {
@@ -67,7 +126,7 @@ export async function addRoute(path, view, accessGroups) {
   const existingRoute = await getRouteByPathOrNull(path);
   if (existingRoute) {
     log.error(LOG_PREFIX, 'route with path already exists', path);
-    throw new Error(`can't create route, route with path already exists: ${path}`);
+    throw new RouteAlreadyExistsError(`can't create route, route with path already exists: ${path}`);
   }
 
   const route = new RouteModel();
@@ -90,7 +149,7 @@ export async function updateRoute(path, newPath, view, accessGroups) {
     const routeWithNewPath = await getRouteByPathOrNull(newPath);
     if (routeWithNewPath) {
       log.error(LOG_PREFIX, 'route with path already exists:', newPath);
-      throw new Error(`can't update route, route with path already exists: ${newPath}`);
+      throw new RouteAlreadyExistsError(`can't update route, route with path already exists: ${newPath}`);
     }
     route.path = newPath;
   }
