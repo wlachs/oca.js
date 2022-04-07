df2d65ea95361bcf585d40d26b4fbae3dad1dc8f
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2022-04-07 20:39:07 +0200

    fix delayed execution

--- client/src/core/utils/delayed_execution.js
+++ client/src/core/utils/delayed_execution.js
@@ -1,5 +1,7 @@
 async function returnAfter(value, afterTime) {
-  return new Promise((resolve) => setTimeout(resolve, afterTime, value));
+  return new Promise((resolve) => {
+    setTimeout(resolve, afterTime, value);
+  });
 }
 
 export default returnAfter;
